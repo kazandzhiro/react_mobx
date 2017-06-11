b8789b229218cf951a62deef20d72ac32c9f5a23
Ivan Kazandzhiev <i.kazandjiev@gmail.com>
2017-06-11 16:38:44 +0300

    Add editor config

---
+++ mocks/api.js
@@ -0,0 +1,33 @@
+export const products = {
+    "products" : [
+        {
+            "name" : "TV",
+            "price" : 1000,
+            "currency" : "USD"
+        },
+        {
+            "name" : "SSD",
+            "price" : 100,
+            "currency" : "USD"
+        }
+    ]
+}
+
+export const users = [
+    { id: 1, name: 'admin', 'permissions' : ['CREATE', 'READ', 'UPDATE', 'DELETE'] },
+    { id: 2, name: 'c', 'permissions' : ['CREATE'] },
+    { id: 3, name: 'r', 'permissions' : ['READ'] },
+    { id: 4, name: 'u', 'permissions' : ['UPDATE'] },
+    { id: 5, name: 'd', 'permissions' : ['DELETE'] },
+    { id: 6, name: 'cr', 'permissions' : ['CREATE', 'READ'] },
+    { id: 7, name: 'cu', 'permissions' : ['CREATE', 'UPDATE'] },
+    { id: 8, name: 'cd', 'permissions' : ['CREATE', 'DELETE'] },  
+    { id: 9, name: 'cru', 'permissions' : ['CREATE', 'READ', 'UPDATE'] },
+    { id: 10, name: 'crd', 'permissions' : ['CREATE', 'READ', 'DELETE'] },
+    { id: 11, name: 'cud', 'permissions' : ['CREATE', 'UPDATE', 'DELETE'] },
+    { id: 12, name: 'rud', 'permissions' : ['READ', 'UPDATE', 'DELETE'] },
+    { id: 13, name: 'ru', 'permissions' : ['READ', 'UPDATE'] },
+    { id: 14, name: 'ru', 'permissions' : ['READ', 'DELETE'] },
+    { id: 15, name: 'ud', 'permissions' : ['UPDATE', 'DELETE'] },
+    { id: 16, name: 'ban', 'permissions' : [] },
+]
