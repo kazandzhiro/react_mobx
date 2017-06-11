c32bb3a5201e4007af5794cc586843952a69d8b2
Ivan Kazandzhiev <i.kazandjiev@gmail.com>
2017-06-11 14:19:18 +0300

    Initial config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react_mobx
 Testing out Mobx and React
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h2>Welcome to CRUD App with MobX</h2>
+        </div>
+    );
+  }
+}
+
+export default App;
